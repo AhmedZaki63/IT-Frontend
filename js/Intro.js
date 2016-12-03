aa7acc2e29965a1dd66b237ff695033849ff77a3
AhmedZaki <ahmed.zaki63@yahoo.com>
2016-12-04 00:46:24 +0200

    Upgrade UI

--- js/Intro.js
+++ js/Intro.js
@@ -1,4 +1,4 @@
-var extend = function(t, e) {
+var extend = function (t, e) {
         function o() {
             this.constructor = t;
         }
@@ -8,7 +8,7 @@ var extend = function(t, e) {
     hasProp = {}.hasOwnProperty;
 
 window.Compass = {
-    init: function() {
+    init: function () {
         "use strict";
         var t;
         return t = new Compass.Router, Backbone.history.start({
@@ -19,102 +19,102 @@ window.Compass = {
     }
 };
 
-$(function() {
+$(function () {
     "use strict";
     return Compass.init();
 });
 
-Compass.Router = function(t) {
+Compass.Router = function (t) {
     function e() {
         return e.__super__.constructor.apply(this, arguments)
     }
-    return extend(e, t), e.prototype.elements = {}, e.prototype.initialize = function() {
+    return extend(e, t), e.prototype.elements = {}, e.prototype.initialize = function () {
         return new IntroSection, this.elements.navigation = new Compass.Views.Navigation
     }, e
 }(Backbone.Router);
 
 var CompassIntro, Dot, coloredDotIndex, ColoredDot, pixelRatio, random, toRad, IntroSection;
-CompassIntro = function() {
+CompassIntro = function () {
     function t(t) {
         var e, o, i;
-        this.el = t, this.dots = [], this.mouse = {}, this.center = {}, this.center.x = this.el.width() / 2, this.center.y = this.el.height() / 3, this.radius = 150, this.density = 15, this.generateDots(), this.generateColoredDots(), e = function(t) {
-            return function(e) {
+        this.el = t, this.dots = [], this.mouse = {}, this.center = {}, this.center.x = this.el.width() / 2, this.center.y = this.el.height() / 3, this.radius = 150, this.density = 15, this.generateDots(), this.generateColoredDots(), e = function (t) {
+            return function (e) {
                 return t.mouse.x = e.offsetX || e.layerX - t.el.offsetLeft || e.pageX, t.mouse.y = e.offsetY || e.layerY - t.el.offsetTop || e.pageY
             }
-        }(this), this.el.mousemove(e), i = function(t) {
-            return function(e) {
+        }(this), this.el.mousemove(e), i = function (t) {
+            return function (e) {
                 var o, i, n, s, r;
                 for (t.center.x = t.el.width() / 2, t.center.y = t.el.height() / 3, s = t.dots, r = [], i = 0, n = s.length; n > i; i++) o = s[i], r.push(o.initPosition());
                 return r
             }
-        }(this), $(window).resize(i), o = function(t) {
-            return function() {
+        }(this), $(window).resize(i), o = function (t) {
+            return function () {
                 var e, i, n, s;
                 for (s = t.dots, i = 0, n = s.length; n > i; i++) e = s[i], e.updateDotMouse();
                 requestAnimationFrame(o)
             }
         }(this), requestAnimationFrame(o)
     }
-    return t.prototype.render = function() {
+    return t.prototype.render = function () {
         return this.el.style.width = "100%", this.el.style.height = "100%", this.el.style.position = "relative"
-    }, t.prototype.generateDots = function() {
+    }, t.prototype.generateDots = function () {
         var t, e, o, i, n, s, r, a;
         for (t = o = 0, r = 720, a = this.density; a > 0 ? r > o : o > r; t = o += a) i = Math.floor(t / 360), s = this.radius + 12 * i, t += i * this.density / 2, n = random(5, 20 - 10 * i), e = new Dot(n, s, this.center, t, this.mouse), this.dots.push(e), this.el.append(e.el)
-    }, t.prototype.generateColoredDots = function() {
+    }, t.prototype.generateColoredDots = function () {
         var t, e, o, i, n, s, r;
         for (e = ["#4A90E2", "#F5A623", "#7ED321", "#D0021B", "#417505"], t = i = 0, r = 360; r > i; t = i += 72) s = this.radius + 5, n = random(25, 35), o = new ColoredDot(e.pop(), n, s, this.center, t, this.mouse), this.dots.push(o), this.el.append(o.el)
     }, t
 }();
 
-Dot = function() {
+Dot = function () {
     function t(t, e, o, i, n) {
         return this.el = document.createElement("li"), this.radius = t, this.centerRadius = e, this.center = o, this.angle = i, this.mouse = n, this.noise = random(-10, 10), this.initPosition(), this.render(), this
     }
-    return t.prototype.render = function() {
+    return t.prototype.render = function () {
         return this.el.style.position = "absolute", this.el.style.width = this.radius + "px", this.el.style.height = this.radius + "px", this.el.style.borderRadius = this.radius + "px", this.el.style.backgroundColor = "black", this.el.style.transform = "translate(" + this.x + "px, " + this.y + "px)"
-    }, t.prototype.initAnimation = function() {
+    }, t.prototype.initAnimation = function () {
         var t, e;
         this.alpha = 0, this.scaleX = .5, this.scaleY = .5, t = this.x, e = this.y, this.updatePosition({
             centerRadius: this.centerRadius + 200
         })
-    }, t.prototype.initPosition = function() {
+    }, t.prototype.initPosition = function () {
         return this.x = this.originalX = this.center.x + this.centerRadius * Math.cos(toRad(this.angle)) + this.noise, this.y = this.originalY = this.center.y + this.centerRadius * Math.sin(toRad(this.angle)) + this.noise
-    }, t.prototype.updatePosition = function(t) {
+    }, t.prototype.updatePosition = function (t) {
         var e, o, i;
         t = t || {}, i = t.centerRadius || this.centerRadius, o = t.center || this.center, e = t.angle || this.angle, this.x = o.x + i * Math.cos(toRad(e)) + this.noise, this.y = o.y + i * Math.sin(toRad(e)) + this.noise
-    }, t.prototype.updateDotMouse = function() {
+    }, t.prototype.updateDotMouse = function () {
         var t, e;
         this.mouse.x && this.mouse.y && (e = Math.atan2(this.y - this.mouse.y, this.x - this.mouse.x), t = 200 / Math.sqrt((this.mouse.x - this.x) * (this.mouse.x - this.x) + (this.mouse.y - this.y) * (this.mouse.y - this.y)), this.x += Math.cos(e) * t + .05 * (this.originalX - this.x), this.y += Math.sin(e) * t + .05 * (this.originalY - this.y), this.el.style.transform = "translate(" + this.x + "px, " + this.y + "px)", this.el.style.webkitTransform = "translate(" + this.x + "px, " + this.y + "px)")
     }, t
 }();
 
 coloredDotIndex = 0;
-ColoredDot = function(t) {
+ColoredDot = function (t) {
     function e(t, o, i, n, s, r) {
         var a;
-        e.__super__.constructor.call(this, o, i, n, s, r), this.el.style.backgroundColor = t, a = this.index = coloredDotIndex, coloredDotIndex++, 0 === this.index && $(this.el).addClass("selected"), this.el.addEventListener("mouseover", function() {
+        e.__super__.constructor.call(this, o, i, n, s, r), this.el.style.backgroundColor = t, a = this.index = coloredDotIndex, coloredDotIndex++, 0 === this.index && $(this.el).addClass("selected"), this.el.addEventListener("mouseover", function () {
             return $(this).addClass("selected"), $(this).siblings().removeClass("selected"), $("#intro .banner li").removeClass("selected").eq(a).addClass("selected")
         })
     }
-    return extend(e, t), e.prototype.updateDotMouse = function() {
+    return extend(e, t), e.prototype.updateDotMouse = function () {
         var t, e;
         this.mouse.x && this.mouse.y && (e = Math.atan2(this.y - this.mouse.y, this.x - this.mouse.x), t = 50 / Math.sqrt((this.mouse.x - this.x) * (this.mouse.x - this.x) + (this.mouse.y - this.y) * (this.mouse.y - this.y)), this.x += Math.cos(e) * t + .05 * (this.originalX - this.x), this.y += Math.sin(e) * t + .05 * (this.originalY - this.y), this.el.style.transform = "translate(" + this.x + "px, " + this.y + "px)", this.el.style.webkitTransform = "translate(" + this.x + "px, " + this.y + "px)")
-    }, e.prototype.show = function() {}, e
+    }, e.prototype.show = function () {}, e
 }(Dot);
 
-random = function(t, e) {
+random = function (t, e) {
     return Math.floor(Math.random() * (e - t + 1) + t)
 };
 
-toRad = function(t) {
+toRad = function (t) {
     return t * Math.PI / 180
 };
 
-IntroSection = function(t) {
+IntroSection = function (t) {
     function e() {
         return e.__super__.constructor.apply(this, arguments)
     }
-    return extend(e, t), e.prototype.el = "#intro", e.prototype.initialize = function() {
+    return extend(e, t), e.prototype.el = "#intro", e.prototype.initialize = function () {
         var t;
         return t = this.$(".dots"), new CompassIntro(t)
     }, e
--- js/script.js
+++ js/script.js
@@ -1,65 +1,75 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     // Scrolling Animation \\
-    $('a.auto-scroll').click(function() {
-        $('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 1500, 'easeInOutExpo');
+    $('a.auto-scroll').click(function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-    
-    $(".navbar").affix({offset: {top: $("#intro").outerHeight(true)} });
+
+    $(".navbar").affix({
+        offset: {
+            top: $("#intro").outerHeight(true)
+        }
+    });
     $('a').css('text-decoration', 'none');
-    
+
     // Workshop Description Viewr \\
-	$('#op').hover(function(){
-            $('#text').html('<font color="#dd3333">LEGOOS</font> Welcome to our magic world, the place where you will be helped to get a masterpiece of your personalities lego with a some of our magical spells.');
-    }, function(){
+    $('#op').hover(function () {
+        $('#text').html('<font color="#dd3333">LEGOOS</font> Welcome to our magic world, the place where you will be helped to get a masterpiece of your personalities lego with a some of our magical spells.');
+    }, function () {
         $('#text').html('WORKSHOPS');
     });
-    
-	$('#op1').hover(function(){
+
+    $('#op1').hover(function () {
         $('#text').html('<font color="#81d742">SugarRush</font> is to show you guys how an android application works , how to make a great looking android application from scratch and the knowledge necessary to do so , but of course with a touch of fun,the way that COMPASS has always done it');
-    }, function(){
+    }, function () {
         $('#text').html('WORKSHOPS');
     });
-    
-	$('#op2').hover(function(){
+
+    $('#op2').hover(function () {
         $('#text').html('<font color="#dd9933">LAYPUZZ</font> A picture is the only way to stop time, it worth a thousand words.stop your time and take your moment make it better and put your own touch on it. in LAYPUZZ workshop, you will get to know how to add you creative touch by using PHOTOSHOP.');
-    }, function(){
+    }, function () {
         $('#text').html('WORKSHOPS');
     });
-    
-	$('#op3').hover(function(){
+
+    $('#op3').hover(function () {
         $('#text').html('<font color="#1e73be">PHAROPELL</font> unleash your inner imagination and strong potentials. It’s the right place to discover your brain’s rainbow of thoughts. You’ll experience different shades of professionality mixed with entertainment and uniqueness. Find your star and hunt it!');
-    }, function(){
+    }, function () {
         $('#text').html('WORKSHOPS');
     });
-    
-	$('#op4').hover(function(){
+
+    $('#op4').hover(function () {
         $('#text').html('<font color="#8224e3">CLICHE</font> Are You Bored From The Routine? Life Routine, Studying Routine & Thinking Routine? Wait A Second! Routine! What Routine? We Do Not Believe That Routine Exists In Our World So Let’s Replace This Thing With Our Unique Creative Touch. Routine Or Creativity? You Are Your Own Life Controller.');
-    }, function(){
+    }, function () {
         $('#text').html('WORKSHOPS');
     });
-    
-	$('#op5').hover(function(){
+
+    $('#op5').hover(function () {
         $('#text').html('<font color="#7c7c7c">2D.NET</font> Web is only the most widely used technology nowadays join us in making contribution to this huge field with our work and designs you will learn all the basics of web design and development while having a good time and enjoying team work and learning in a new way that will make you so eager to learn.');
-    }, function(){
+    }, function () {
         $('#text').html('WORKSHOPS');
     });
-    
+
     // News Tab \\
-    $('.close').click(function(){
+    $('.close').click(function () {
         $('.news-background').hide(200);
     });
-    $('#blog .news-list .news').click(function(){
+    $('#blog .news-list .news').click(function () {
         $('.news-background').show(200);
         var title = $(this).find('h3').text();
         var img = $(this).find('img').attr('src');
         var des = $(this).find('p').text();
         $('.news-tab h1').text(title);
         $('.news-tab img').attr('src', img);
-        $('.news-tab h3').text(des);
+        $('.news-tab h4').text(des);
     });
-    
+    $(document).mouseup(function (e) {
+        if (!$(".news-tab").is(e.target) && $(".news-tab").has(e.target).length === 0)
+            $('.news-background').hide(200);
+    });
+
     // Slick Animation \\
     $('.news-list').slick({
         slidesToShow: 3,
@@ -67,16 +77,20 @@ $(document).ready(function(){
         dots: true,
         responsive: [
             {
-              breakpoint: 992,
-              settings: {slidesToShow: 2}
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
             },
             {
-              breakpoint: 650,
-              settings: {slidesToShow: 1}
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1
+                }
             }
         ]
-    });   
-    
+    });
+
     $('[data-toggle="popover"]').popover();
     /*
     $('#text').fadeOut(300, function() {
@@ -84,30 +98,31 @@ $(document).ready(function(){
     });
     */
 });
-//////////////////////////////////////////////////////////////////////////////
-$("#about .a").click(function(){
-    $("#about .x").css("display","block");
-    $("#about .y").css("display","none");
-    $("#about .z").css("display","none");
-    $("#about .a").css("background-color","#e7e7e7");
-    $("#about .b").css("background-color","#fff");
-    $("#about .c").css("background-color","#fff");
- });
-$("#about .b").click(function(){
-    $("#about .y").css("display","block");
-    $("#about .x").css("display","none");
-    $("#about .z").css("display","none");
-    $("#about .b").css("background-color","#e7e7e7");
-    $("#about .a").css("background-color","#fff");
-    $("#about .c").css("background-color","#fff");
-});
-$("#about .c").click(function(){
 
-    $("#about .z").css("display","block");
-    $("#about .y").css("display","none");
-    $("#about .x").css("display","none");
-    $("#about .c").css("background-color","#e7e7e7");
-    $("#about .a").css("background-color","#fff");
-    $("#about .b").css("background-color","#fff");
+//////////////////////////////////////////////////////////////////////////////
+$("#about .a").click(function () {
+    $("#about .x").addClass("select")
+    $("#about .y").removeClass("select");
+    $("#about .z").removeClass("select");
+    $("#about .a").css("background-color", "#e7e7e7");
+    $("#about .b").css("background-color", "#fff");
+    $("#about .c").css("background-color", "#fff");
+});
+$("#about .b").click(function () {
+    $("#about .y").addClass("select")
+    $("#about .x").removeClass("select");
+    $("#about .z").removeClass("select");
+    $("#about .b").css("background-color", "#e7e7e7");
+    $("#about .a").css("background-color", "#fff");
+    $("#about .c").css("background-color", "#fff");
+});
+$("#about .c").click(function () {
+
+    $("#about .z").addClass("select")
+    $("#about .y").removeClass("select");
+    $("#about .x").removeClass("select");
+    $("#about .c").css("background-color", "#e7e7e7");
+    $("#about .a").css("background-color", "#fff");
+    $("#about .b").css("background-color", "#fff");
 
 });
